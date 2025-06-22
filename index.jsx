161778220ab5590958f6ea59ef499923a51bccec
Petros Pikionis <petrospikionis@gmail.com>
2025-06-22 09:57:32 +0300

    add  link and Routes  on  home page

--- index.jsx
+++ index.jsx
@@ -3,6 +3,8 @@ import ReactDOM from 'react-dom/client';
 import { BrowserRouter, Routes, Route, Link } from "react-router-dom"
 import Home from "./pages/Home"
 import About from "./pages/About"
+import About from "./pages/Route"
+import About from "./pages/Link"
 
 function App() {
   return (
@@ -11,11 +13,15 @@ function App() {
         <Link className="site-logo" to="/">#VanLife</Link>
         <nav>
           <Link to="/about">About</Link>
+          <Link to="/Route">Route</Link>
+          <Link to="/Link">Link</Link>
         </nav>
       </header>
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/about" element={<About />} />
+        <Route path="/Route" element={<Route />} />
+        <Route path="/Link" element={<Link />} />
       </Routes>
     </BrowserRouter>
   )
